2961198af6d77150c0b9f0ae3dede8e435614353
James Smith <james_smith.games@live.co.uk>
2017-01-21 00:40:01 +0000

    Basic waving mechanic working

---
+++ GGJ - WaveSim Unity Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float satisfaction;
+    public int targetSatisfaction = 100;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GGJ - WaveSim Unity Project/Assets/Scripts/MouseWave.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWave : MonoBehaviour {
+
+    public LayerMask enemyLayer;
+
+    float xMovement, yMovement;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin,ray.direction);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, enemyLayer)) {
+            Enemy enemy = hit.collider.GetComponent<Enemy>();
+
+            xMovement = Input.GetAxis("Mouse X");
+            yMovement = Input.GetAxis("Mouse Y");
+
+            enemy.satisfaction += 1 + xMovement + yMovement;
+            //enemy.satisfaction++;
+
+        }
+
+	}
+}
