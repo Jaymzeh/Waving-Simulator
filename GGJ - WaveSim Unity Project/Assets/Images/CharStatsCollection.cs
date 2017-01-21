dcfe5ecfc1efa8e29b01f1254f0e8e3b0754eaf4
viscountess <venerable_viscountess@hotmail.com>
2017-01-21 11:54:00 +0000

    Initial Character Stats setup

---
+++ GGJ - WaveSim Unity Project/Assets/Images/CharStatsCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStatsCollection
+{
+    private Dictionary<CharStatsType, CharStats> _statDict;
+
+    public CharStatsCollection()
+    {
+        _statDict = new Dictionary<CharStatsType, CharStats>();
+    }
+}
---
+++ GGJ - WaveSim Unity Project/Assets/Images/CharStatsType.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharStatsType{
+    WavePower = 2;
+    Mobility = 2;
+    HandSize = 1;
+	
+}
