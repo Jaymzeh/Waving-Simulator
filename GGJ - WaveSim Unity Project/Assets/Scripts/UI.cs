f3160fc10c9420d3f9cefdf0e397fc93d0355b33
James Smith <james_smith.games@live.co.uk>
2017-01-21 11:04:49 +0000

    Window Panning done

--- GGJ - WaveSim Unity Project/Assets/Scripts/UI.cs
+++ GGJ - WaveSim Unity Project/Assets/Scripts/UI.cs
@@ -3,8 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UI : MonoBehaviour {
+    GameObject[] backgrounds;
+    public static UI instance;
+    
+    void Awake() {
+        instance = this;
+        instance.backgrounds = GameObject.FindGameObjectsWithTag("Background");
+    }
 
 	public void ChangeScene(string scene) {
         LoadingScreen.ChangeScene(scene);
+    }
+
+
+    public void PanWindows(Vector3 direction) {
+        foreach(GameObject image in instance.backgrounds) {
+            image.transform.Translate(direction);
+        }
     }
 }
