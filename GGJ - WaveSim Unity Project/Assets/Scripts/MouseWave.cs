b33933ec876428e6c27d5df3a0c80e25d220819d
James Smith <james_smith.games@live.co.uk>
2017-01-21 02:00:43 +0000

    Hand waving autonomously.

--- GGJ - WaveSim Unity Project/Assets/Scripts/MouseWave.cs
+++ GGJ - WaveSim Unity Project/Assets/Scripts/MouseWave.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MouseWave : MonoBehaviour {
 
     public LayerMask enemyLayer;
+    public float slowWave = 0.25f;
+    public float fastWave = 3;
+    int direction = 1;
 
     float xMovement, yMovement;
 
@@ -17,11 +20,25 @@ public class MouseWave : MonoBehaviour {
         Debug.DrawRay(ray.origin,ray.direction);
         RaycastHit hit;
 
+        
+
         if (Physics.Raycast(ray, out hit, enemyLayer)) {
+
+            if (transform.localEulerAngles.z > 110 || transform.eulerAngles.z < 70)
+            direction *= -1;
+        
+
             Enemy enemy = hit.collider.GetComponent<Enemy>();
 
             xMovement = Input.GetAxis("Mouse X");
             yMovement = Input.GetAxis("Mouse Y");
+
+            Debug.Log("Mouse movement[" + xMovement + "," + yMovement + "]");
+            Debug.Log("Total["+xMovement+yMovement+"]");
+            if(xMovement+yMovement <0.25)
+                transform.Rotate(0, 0, direction*slowWave);
+            else
+                transform.Rotate(0, 0, direction*fastWave);
 
             enemy.satisfaction += 1 + xMovement + yMovement;
             //enemy.satisfaction++;
