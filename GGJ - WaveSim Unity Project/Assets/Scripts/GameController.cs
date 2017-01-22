e2cabd74b01db1e8a0039cfbfdf7a6680f13f865
James Smith <james_smith.games@live.co.uk>
2017-01-22 09:56:32 +0000

    Enemies talk when spawning and being killed. Only one audio source so they don't overlap with each other.

--- GGJ - WaveSim Unity Project/Assets/Scripts/GameController.cs
+++ GGJ - WaveSim Unity Project/Assets/Scripts/GameController.cs
@@ -11,6 +11,7 @@ public class GameController : MonoBehaviour {
     public static bool InputEnabled = true;
     public static bool Paused = false;
     public GameObject gameOver;
+    public AudioClip newHighScoreClip;
     public static int score;
     public static int highScore;
 
@@ -24,9 +25,20 @@ public class GameController : MonoBehaviour {
 
     public static void ShowGameOver() {
         Paused = true;
-        if (score > highScore)
+        GameObject go = Instantiate(instance.gameOver);
+        if (score > highScore) {
             highScore = score;
-        Instantiate(instance.gameOver);
+            AudioSource source = go.GetComponent<AudioSource>();
+            instance.StartCoroutine("PlayNewHighScore", source);
+        }
+    }
+
+    IEnumerator PlayNewHighScore(AudioSource source) {
+        while (source.isPlaying) {
+            yield return null;
+        }
+        Debug.Log("Playing new high score sound");
+        source.clip = newHighScoreClip;
     }
 
     public static int ReadHighScore() {
@@ -36,19 +48,23 @@ public class GameController : MonoBehaviour {
             StreamReader sr = File.OpenText(file);
             string line = sr.ReadLine();
             Int32.TryParse(line, out score);
-
         }
         return score;
     }
-
-    public static void ExitGame() {
+    public static void SaveHighScore(int newScore) {
         string filename = "Highscore.txt";
         if (File.Exists(filename)) {
             Debug.Log(filename + " already exists");
             return;
         }
         StreamWriter sr = File.CreateText(filename);
-        sr.WriteLine(highScore);
+        sr.WriteLine(newScore);
         sr.Close();
+    }
+
+    public static void ExitGame() {
+        if (score > highScore)
+            SaveHighScore(score);
+        Application.Quit();
     }
 }
--- GGJ - WaveSim Unity Project/Assets/Scripts/ShowScore.cs
+++ GGJ - WaveSim Unity Project/Assets/Scripts/ShowScore.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 
 public class ShowScore : MonoBehaviour {
 
-    Text text;
+    public Text scoreText, highScoreText;
 
 	void Start () {
-        text = GetComponent<Text>();
+        if (highScoreText != null)
+            GameController.ReadHighScore();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        text.text = "Score: " + GameController.score.ToString();
+        if (scoreText != null)
+            scoreText.text = "Score: " + GameController.score.ToString();
+
+        if (highScoreText != null)
+            highScoreText.text = "Highscore: " + GameController.highScore.ToString();
 	}
 }
