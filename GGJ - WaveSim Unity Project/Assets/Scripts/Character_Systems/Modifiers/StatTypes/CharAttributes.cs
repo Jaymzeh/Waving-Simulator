94bbea64b69cc65679ca9937028f99ed03fefa45
viscountess <venerable_viscountess@hotmail.com>
2017-01-22 08:51:48 +0000

    Adding in scalable attributes

---
+++ GGJ - WaveSim Unity Project/Assets/Scripts/Character_Systems/Modifiers/StatTypes/CharAttributes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CharAttributes: CharStatModifiable, IStatScalable
+{
+    private int _statLevelValue;
+
+    public int StatLevelValue
+    {
+        get { return _statLevelValue; }
+    }
+
+    public override int StatBaseValue
+    {
+        get { return base.StatBaseValue + StatLevelValue; }
+    }
+
+    public void ScaleStat(int level)
+    {
+        _statLevelValue = level;
+    }
+}
